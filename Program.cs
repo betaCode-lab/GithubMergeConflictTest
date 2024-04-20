9281f7aeb7f9d7e9b8274cbc3c3accb315a65cd3
Adrian Bañuelas <adriangpe5666@gmail.com>
2024-04-20 11:15:08 -0600

    Creating new merge conflicts

--- Program.cs
+++ Program.cs
@@ -5,6 +5,6 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
-        Console.WriteLine("Chanchito feliz con nuevos cambios");
+        Console.WriteLine("Chanchito feliz");
     }
 }
